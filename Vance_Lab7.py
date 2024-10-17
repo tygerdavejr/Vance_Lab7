2a595febd84a3a5ad0c676f9e2824926a1a549f3
David Vance <dvance4@mail.pima.edu>
2024-10-17 11:52:47 -0700

    Lab 7-3
    Start with Steps 1-3, which includes building the shell for the dice roller game.

---
+++ Vance_Lab7.py
@@ -0,0 +1,36 @@
+# David Vance
+# Professor Kevin Chang
+# CIS129 - Programming and Problem Solving I
+
+"""This program does a dice game"""
+
+# Lab 7-3 The Dice Game
+# add libraries needed
+
+# the main function
+def main():
+    print()
+
+    # initialize variables
+
+    # call to inputNames
+
+    # while loop to run program again
+    while endProgram =='no':
+
+        # populate variables
+
+        # call to rollDice
+
+        # call to displayInfo
+
+        endProgram = input('Do you want to end program? (yes/no): ')
+
+# this function gets the player's names
+
+# this fuction will get the random values
+
+# this function displays the winner
+
+# calls main
+main()
